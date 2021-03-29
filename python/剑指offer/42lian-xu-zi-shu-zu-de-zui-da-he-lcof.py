096f334bcb0eeb747a14ee9953a5347070ba0dfb
chensy <1278651995@qq.com>
2021-03-29 16:50:13 +0800

    剑指 Offer 42. 连续子数组的最大和

---
+++ python/剑指offer/42lian-xu-zi-shu-zu-de-zui-da-he-lcof.py
@@ -0,0 +1,25 @@
+# coding=utf8
+from typing import List
+
+"""
+剑指 Offer 42. 连续子数组的最大和
+https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/
+"""
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        pre = 0
+        res = nums[0]
+        cur = 0
+        for i in nums:
+            cur = i
+            if pre > 0:
+                cur += pre
+            if cur > res:
+                res = cur
+            pre = cur
+        return res
+
+
+if __name__ == '__main__':
+    print(Solution().maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
